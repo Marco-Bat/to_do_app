248d7ea40caf16b84944d23612a1c2d987da13e5
Marco-Bat <marcobattimelli@gmail.com>
2024-09-09 10:42:28 +0200

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
 
+app = create_app()#the returned object from create_app with all config
 
 if __name__=="__main__":
-    app = create_app()
     app.run (debug=True, port=8000)
 
